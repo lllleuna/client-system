ac99a22733c4864631a4eca598ec8ec2f02debc6
Leunamme Atutubo <leunammerosev.atutubo@gmail.com>
2024-12-02 22:50:10 +0800

    fix: button style

--- resources/views/components/cancel-button.blade.php
+++ resources/views/components/cancel-button.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['class' => 'my-1 px-5 py-1 rounded text-gray-800 hover:bg-gray-200 focus:outline-none transition-colors'])}} >
+<button {{ $attributes->merge(['class' => 'my-1 px-5 py-1 text-red-900 bg-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-md text-center'])}} >
 
     {{ $slot }}
 </button>
--- resources/views/components/form-submit-button.blade.php
+++ resources/views/components/form-submit-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'my-1 px-5 py-1 rounded bg-blue-900 text-white hover:bg-blue-700 focus:outline-none transition-colors', 'type' => 'submit'])}}>
+<button {{ $attributes->merge(['class' => 'my-1 px-5 py-1 text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-md text-center ', 'type' => 'submit'])}}>
     {{ $slot }}
 </button>

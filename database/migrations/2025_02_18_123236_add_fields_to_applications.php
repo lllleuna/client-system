dd75d3deaf929691965059bb49ee3132f5c313a7
Leunamme <leuna.atutubo@gmail.com>
2025-02-27 13:34:00 +0800

    application migration update

---
+++ database/migrations/2025_02_18_123236_add_fields_to_applications.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('file_upload')->required();
+            $table->string('reference_number')->unique();
+        });
+    }
+
+
+    // run the below command in php my admin if this migration did not work
+    
+// UPDATE applications 
+// SET reference_number = CONCAT('APP-', LPAD(id, 6, '0'))
+// WHERE reference_number IS NULL OR reference_number = '';
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            //
+        });
+    }
+};

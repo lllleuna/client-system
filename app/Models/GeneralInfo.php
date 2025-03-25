bace9d84d9e0a58dd063132a42af7ca321395b14
Leunamme <leuna.atutubo@gmail.com>
2025-03-25 11:20:14 +0800

    cgshistory

--- app/Models/GeneralInfo.php
+++ app/Models/GeneralInfo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class GeneralInfo extends Model
 {
     protected $table='general_info';
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'validity_date' => 'date',
+    ];
 }

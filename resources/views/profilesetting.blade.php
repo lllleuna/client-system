f7f42f41e394801cb856108c96c492766ef80c81
Leunamme <leuna.atutubo@gmail.com>
2025-04-14 03:01:59 +0800

    header layout fixed

--- resources/views/profilesetting.blade.php
+++ resources/views/profilesetting.blade.php
@@ -11,8 +11,16 @@
 
         {{-- Profile Picture/Logo Card --}}
         <div class="bg-white rounded-xl shadow-md overflow-hidden mb-8">
-            <div class="bg-gray-50 px-4 py-5 border-b border-gray-200">
-                <h3 class="text-lg font-medium text-gray-900">Cooperative Logo</h3>
+            <div class="bg-gradient-to-r from-blue-50 to-indigo-50 px-6 py-5 border-b border-gray-200">
+                <h3 class="text-lg font-medium text-gray-900 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-blue-600" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    Cooperative Logo
+                </h3>
             </div>
             <div class="px-4 py-8 flex flex-col items-center">
                 <div class="relative group">
@@ -44,8 +52,16 @@
 
 
         <div class="bg-white rounded-xl shadow-md overflow-hidden mb-8">
-            <div class="bg-gray-50 px-4 py-5 border-b border-gray-200">
-                <h3 class="text-lg font-medium text-gray-900">Account Password</h3>
+            <div class="bg-gradient-to-r from-blue-50 to-indigo-50 px-6 py-5 border-b border-gray-200">
+                <h3 class="text-lg font-medium text-gray-900 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-blue-600" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    Account Password
+                </h3>
             </div>
 
             <form method="POST" action="{{ route('profile.updatePassword') }}" class="px-4 py-8 w-full">
@@ -147,8 +163,8 @@
                     <div class="flex flex-col items-center space-y-5 py-4">
                         <div class="flex items-center justify-center w-16 h-16 rounded-full bg-gray-100">
                             @if ($user->two_factor_enabled)
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-green-600" viewBox="0 0 20 20"
-                                    fill="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-green-600"
+                                    viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd"
                                         d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                                         clip-rule="evenodd" />

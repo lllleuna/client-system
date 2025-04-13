63ef85e801a53bf98689133f587fab40b0c4e90f
Leunamme <leuna.atutubo@gmail.com>
2025-04-14 03:15:46 +0800

    name layout

--- resources/views/profilesetting.blade.php
+++ resources/views/profilesetting.blade.php
@@ -36,10 +36,8 @@
                         </button>
                     </div>
                 </div>
-                <h3 class="flex items-center space-x-2 py-2">
-                    <span class="text-gray-800 text-xl font-medium leading-tight uppercase">
-                        {{ Auth::user()->tc_name ?? 'Cooperative Chairperson' }}
-                    </span>
+                <h3 class="flex items-center mt-2 space-x-2 py-2 uppercase">
+                    {{ Auth::user()->tc_name ?? 'Cooperative Chairperson' }}
                 </h3>
             </div>
         </div>
@@ -157,8 +155,8 @@
                     <div class="flex flex-col items-center space-y-5 py-4">
                         <div class="flex items-center justify-center w-16 h-16 rounded-full bg-gray-100">
                             @if ($user->two_factor_enabled)
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-green-600"
-                                    viewBox="0 0 20 20" fill="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-green-600" viewBox="0 0 20 20"
+                                    fill="currentColor">
                                     <path fill-rule="evenodd"
                                         d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                                         clip-rule="evenodd" />

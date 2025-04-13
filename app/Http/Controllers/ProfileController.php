37fa09ff442d573fb4cbf7027cf1e734278a9037
Leunamme <leuna.atutubo@gmail.com>
2025-04-14 00:21:52 +0800

    removed -> on Auth user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,7 +20,7 @@ class ProfileController extends Controller
             'profile_picture' => 'required|image|mimes:jpeg,png|max:2048',
         ]);
 
-        $user = Auth::->user();
+        $user = Auth::user();
 
         $file = $request->file('profile_picture');
         $filename = 'profile_' . $user->id . '.' . $file->getClientOriginalExtension();

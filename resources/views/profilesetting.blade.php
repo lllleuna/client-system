9b27569cf05d4122b988a7d63629ca5c9bc8a1ad
Leunamme <leuna.atutubo@gmail.com>
2025-04-14 03:10:13 +0800

    layout pic fixed

--- resources/views/profilesetting.blade.php
+++ resources/views/profilesetting.blade.php
@@ -25,7 +25,7 @@
             <div class="px-4 py-8 flex flex-col items-center">
                 <div class="relative group">
                     <img src="{{ $user->profile_picture ? asset('shared/uploads/' . $user->profile_picture) : asset('images/default.png') }}"
-                        class="h-40 w-40 object-cover rounded-full ring-4 ring-gray-100 shadow-md" alt="Cooperative Logo">
+                        class="h-40 w-40 object-cover rounded-full ring-4 ring-black-400 shadow-md" alt="Cooperative Logo">
                     <div
                         class="absolute inset-0 rounded-full bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity flex items-center justify-center">
                         <button
@@ -36,17 +36,6 @@
                         </button>
                     </div>
                 </div>
-
-                <button
-                    class="mt-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    type="button" onclick="document.getElementById('editProfilePicModal').classList.remove('hidden')">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                    </svg>
-                    Update Logo
-                </button>
             </div>
         </div>
 

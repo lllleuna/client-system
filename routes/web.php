24e6a4dec50759a422faed1794ee654417117117
Leunamme <leuna.atutubo@gmail.com>
2025-04-20 16:22:35 +0800

    /users/create

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::post('/profile/toggle-2fa', [ProfileController::class, 'toggleTwoFactor']
 
 
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-Route::get('users/create', [RegisteredUserController::class, 'show'])->middleware('auth');
+Route::get('users/create', [RegisteredUserController::class, 'show']);
 Route::post('users/create', [RegisteredUserController::class, 'store']);
 
 
